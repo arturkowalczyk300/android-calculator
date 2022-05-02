d4e1b81fbef3076c03953b7f366a5659ce1a8da9
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-05-02 13:46:37 +0200

    use styles in layout, add onclick listener

--- app/src/main/java/com/arturkowalczyk300/calculator/MainActivity.kt
+++ app/src/main/java/com/arturkowalczyk300/calculator/MainActivity.kt
@@ -2,10 +2,22 @@ package com.arturkowalczyk300.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //var exampleBtn: Button = findViewById(R.id.btnAC)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //exampleBtn.setOnClickListener(fc())
     }
+
+    fun buttonOnClickListener(view: View){Log.v(
+        "myApp",
+        "clicked button with tag:${view.getTag()}"
+    )}
+
 }
